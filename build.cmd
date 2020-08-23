25b4b99384de25aa0289e633b229d4e545ba34c4
alexshavlovsky <ctzn@tut.by>
2020-08-23 12:08:28 +0300

    remove unnecessary native bins before build

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
 git clone https://github.com/alexshavlovsky/primeng-chat-client.git
 git clone https://github.com/alexshavlovsky/spring-mongo-reactive-chat.git
+del .\spring-mongo-reactive-chat\src\main\resources\nativebin\ffmpeg-amd64.exe
+del .\spring-mongo-reactive-chat\src\main\resources\nativebin\ffmpeg-x86_64-osx
 cd .\spring-mongo-reactive-chat
 call mvn clean package
 cd ..
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/alexshavlovsky/primeng-chat-client.git
 git clone https://github.com/alexshavlovsky/spring-mongo-reactive-chat.git
+rm ./spring-mongo-reactive-chat/src/main/resources/nativebin/ffmpeg-amd64.exe
+rm ./spring-mongo-reactive-chat/src/main/resources/nativebin/ffmpeg-x86_64-osx
 cd ./spring-mongo-reactive-chat || exit
 mvn clean package
 cd ..
