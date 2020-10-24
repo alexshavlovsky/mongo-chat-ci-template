5b8b79a150f2818989003d762eedcf3ce6516b5a
alexshavlovsky <ctzn@tut.by>
2020-10-24 20:21:24 +0300

    fix the windows command file

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 git clone https://github.com/alexshavlovsky/primeng-chat-client.git
 git clone https://github.com/alexshavlovsky/spring-mongo-reactive-chat.git
-del .\spring-mongo-reactive-chat\src\main\resources\nativebin\ffmpeg-amd64.exe
+del .\spring-mongo-reactive-chat\src\main\resources\nativebin\ffmpeg-amd64
 del .\spring-mongo-reactive-chat\src\main\resources\nativebin\ffmpeg-x86_64-osx
 cd .\spring-mongo-reactive-chat
 call mvn clean package
